825f7b549613d8fd91db411b990c02e915a5153b
Victor Alcantara <victorjnio75@gmail.com>
2022-06-22 13:44:13 -0300

    refactor

--- secutiry/src/main/java/com/secutiry/security/filters/CustomAuthorizationFilter.java
+++ secutiry/src/main/java/com/secutiry/security/filters/CustomAuthorizationFilter.java
@@ -14,14 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.secutiry.security.SecurityUtil;
 
 public class CustomAuthorizationFilter extends OncePerRequestFilter{
@@ -31,9 +29,12 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter{
             throws ServletException, IOException {
         
         if (!request.getServletPath().equals("/api/login") && !request.getServletPath().equals("/api/token/refresh")){
+
             String authorizationHeader = request.getHeader("Authorization");
+            
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
                 try {
+
                     String token = authorizationHeader.substring("Bearer ".length());
                     DecodedJWT decodedJWT = SecurityUtil.decodeJwt(token);
                     String username = decodedJWT.getSubject();
@@ -47,9 +48,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter{
                     response.setStatus(HttpStatus.FORBIDDEN.value());
                     Map<String, String> error = new HashMap<>();
                     error.put("error", "Invalid token!");
-                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                    new ObjectMapper().writeValue(response.getOutputStream(), error);
-            
+                    SecurityUtil.writeResponseBody(response, error);
                 }
             }
         }
